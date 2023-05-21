a7ee567e5fa7d221cbdae64c093665dc21411d8d
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-05-21 22:36:53 +0530

    Added a AuthContext file and used it as wrapper

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState,useReducer,useEffect} from 'react';
+import React, { useState,useReducer} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
